63e97f9d7e3d648a2044cc0d234058322d3cee19
Khem Veasna <kveasna@gmail.com>
2016-02-03 16:29:06 -0500

    added test app

---
+++ testapplication/src/androidTest/java/testapplication/android/laidbacklife/net/test/ApplicationTest.java
@@ -0,0 +1,16 @@
+package testapplication.android.laidbacklife.net.test;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+import junit.framework.Assert;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+
+}
