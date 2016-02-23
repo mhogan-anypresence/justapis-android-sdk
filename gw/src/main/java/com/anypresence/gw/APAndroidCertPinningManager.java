99d6c97256a1722adf04e2a4c77d7e67ecdd1024
Khem Veasna <kveasna@gmail.com>
2016-02-23 17:27:51 -0500

    [#114356395] Add method that can take a cert as a byte array

--- gw/src/main/java/com/anypresence/gw/APAndroidCertPinningManager.java
+++ gw/src/main/java/com/anypresence/gw/APAndroidCertPinningManager.java
@@ -5,8 +5,14 @@ import android.util.Base64;
 import com.squareup.okhttp.CertificatePinner;
 import com.squareup.okhttp.OkHttpClient;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.Principal;
 import java.util.HashMap;
 import java.util.Map;
+
+import javax.security.cert.CertificateException;
+import javax.security.cert.X509Certificate;
 
 public class APAndroidCertPinningManager {
     private static APAndroidCertPinningManager manager;
@@ -24,9 +30,7 @@ public class APAndroidCertPinningManager {
 
 
     /**
-     * Adds cert for provided hostname
-     *
-     * a base64 Subject Public Key Info is needed that starts with 'sha1/'
+     * Adds cert for provided hostname.
      *
      * Some reading material https://www.imperialviolet.org/2011/05/04/pinning.html
      *
@@ -35,10 +39,26 @@ public class APAndroidCertPinningManager {
      * #add("google.com", "sha1/qANMQh2fy6tyyjS9qEjosxOLe1w=")
      *
      * @param hostname
-     * @param sha1
+     * @param sha1 base64 Subject Public Key Info is needed that starts with 'sha1/'
      */
     public void add(String hostname, String sha1) {
         certs.put(hostname, sha1);
+    }
+
+    /**
+     * Adds cert for provided hostname.
+     *
+     * @param hostname
+     * @param data the certficate file as a byte array
+     */
+    public void add(String hostname, byte[] data) {
+        try {
+            certs.put(hostname, rehashCertFromByteArray(data));
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (CertificateException e) {
+            e.printStackTrace();
+        }
     }
 
     public Map<String,String> getCerts() {
@@ -52,5 +72,15 @@ public class APAndroidCertPinningManager {
             builder.add(entry.getKey(), entry.getValue());
         }
         client.setCertificatePinner(builder.build());
+    }
+
+    private String rehashCertFromByteArray(byte[] data) throws NoSuchAlgorithmException, CertificateException {
+        X509Certificate cert = X509Certificate.getInstance(data);
+        byte[] pubKey = cert.getPublicKey().getEncoded();
+
+        MessageDigest digest = MessageDigest.getInstance("SHA-1");
+        final byte[] hash = digest.digest(pubKey);
+
+        return "sha1/" + Base64.encodeToString(hash, Base64.NO_PADDING);
     }
 }
